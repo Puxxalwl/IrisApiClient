976ddf12410874c2d2264a89fb0a59510cb0fec3
puxxalwl <puxxalwl98@gmail.com>
2025-08-05 22:30:15 +0300

    Add new interface result

---
+++ IrisClient/Models/Result.cs
@@ -0,0 +1,9 @@
+namespace IrisClient.Models
+{
+    public class Result
+    {
+        public string? ok { get; set; }
+        public string? response { get; set; }
+        public string? result { get; set; }
+    }
+}
