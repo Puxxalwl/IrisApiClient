e39d22bd67be41d8b0543d21716dfc7eae273289
puxxalwl <puxxalwl98@gmail.com>
2025-08-05 18:01:25 +0300

    Example IrisApi

---
+++ Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using IrisClient.Config;
+using IrisClient.Services;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var client = IrisApiClient.Create();
+
+            var balance = await client.GetBalance();
+
+            Console.WriteLine($"Ирис-голд: {balance.gold}, Ириски: {balance.sweets}, Очки-Доната: {balance.donate_score}");
+        }
+        catch (Exception err)
+        {
+            Console.WriteLine($"Ошибка: {err.Message}");
+        }
+    }
+}
